67f41cb2fee32549bcd39bcc98437ff225d2df9e
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-02 15:41:14 +0900

    feat : make Counter class
    use increment method

--- 6.quiz1.js
+++ 6.quiz1.js
@@ -2,19 +2,30 @@
 // 0 이상의 값으로 초기화 한 뒤 하나씩 숫자를 증가할 수 있는 카운터를 만들기
 // Counter 클래스 만들기
 
+// class Counter {
+//   num = 0;
+
+//   constructor(count) {
+//     this.count = count;
+//   }
+
+//   increase() {
+//     for (let i = 0; i < this.count; i++) {
+//       num++;
+//     }
+//     return console.log(`${this.num}번 까지 카운팅 완료했습니다!`);
+//   }
+// }
+
+// const counter = new Counter(0);
+
 class Counter {
-  num = 0;
-
-  constructor(count) {
-    this.count = count;
-  }
-
-  increase() {
-    for (let i = 0; i < this.count; i++) {
-      num++;
+  #value;
+  constructor(startValue) {
+    if (isNaN(startValue) || startValue < 0) {
+      this.#value = 0;
+    } else {
+      this.#value = startValue;
     }
-    return console.log(`${this.num}번 까지 카운팅 완료했습니다!`);
   }
 }
-
-const counter = new Counter(0);
