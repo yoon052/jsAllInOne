902a8e7397f46bd8118c47e935733a47043c3423
yoon052 <105267838+yoon052@users.noreply.github.com>
2023-08-06 12:47:09 +0900

    feat : module

---
+++ 12.module/main.js
@@ -0,0 +1,8 @@
+// import { increase as increase1 } from "./counter.js";
+import { increase, getCount } from "./counter.js";
+increase();
+increase();
+increase();
+
+const count = getCount();
+console.log(count);
